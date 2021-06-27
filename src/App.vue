e1029ebcecd2786657b18e4e4dca66e7aae1deb3
leegise-lab <dlvpslr@naver.com>
2021-06-27 18:31:17 +0900

    vue 프로젝트 생성

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrap">
+    <div class="wrap-content">
+        <div class="wrap-list">
+            <canvas class="stat-model" id="stat-model">
+            </canvas>
+        </div>
+        <div class="wrap-list">
+            <a class="list">
+                성장 과정.
+            </a>
+            <a class="list">
+                Portfolio.
+            </a>
+            <a class="list">
+                Contact.
+            </a>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.wrap {
+    width: 100%;
+    height: 100vh;
+    background-color: #353535;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .wrap-content {
+        width: 90%;
+        height: 90%;
+        display: flex;
+        border: 1px solid wheat;  
+        .content {
+            width: 100%;
+            height: 100%;
+            border: 1px solid red;
+        }
+        .wrap-list {
+            width: 100%;
+            height: 100%;
+            align-items: center;
+            .list {
+                width: 90%;
+                height: 10vh;
+                background-color: whitesmoke;
+                border: 5px solid black;
+                border-radius: 10px;
+                margin: 0 auto;
+                margin-top: 50px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+            .list:hover {
+                cursor: pointer;
+            }
+        }
+    }
 }
 </style>
